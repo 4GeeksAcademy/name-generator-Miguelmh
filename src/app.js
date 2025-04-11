e12126c6709a85a293c9372c005dfd212629687a
Dolphinh16 <miguelhmercedes@hotmail.com>
2025-04-11 16:07:23 +0000

    Generador the dominiommh

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  for ( let i=0; i< pronoun.length; i++){
+    
+
+    for (let j=0; j< adj.length; j++) {
+      for (let h=0; h< noun.length; h++){
+        console.log(pronoun[i] + adj [j] + noun [h] + ".com")
+     
+
+      }
+      
+     
+
+
+    }
+
+
+
+  }
+
   console.log("Hello Rigo from the console!");
 };
